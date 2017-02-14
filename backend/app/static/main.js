1d72e2259e4f644cdf271e68a29987d87f8a0c28
James Ryan <joryan@ucsc.edu>
2017-02-14 07:04:36 -0800

    Duct tape for race conditions around closedModal method

--- backend/app/static/main.js
+++ backend/app/static/main.js
@@ -299,7 +299,8 @@ Main.prototype.init = function(){
 
     // Raycasting code for game selection on touchscreen devices
 	document.addEventListener("touchend", function(e){
-		if( that.closedModal() && !that.moving() && !that.leftJoystick.anyInput() && !that.rightJoystick.anyInput() ){
+//		if( that.closedModal() && !that.moving() && !that.leftJoystick.anyInput() && !that.rightJoystick.anyInput() ){
+        if( that.launched && !that.moving() && !that.leftJoystick.anyInput() && !that.rightJoystick.anyInput() ){
             if((  that.selected == null || ((that.mousePos.y < that.height/2 - that.paneWidth/2 - that.paneDelta) || (that.mousePos.x < that.width/2 - that.paneWidth/2 - that.paneDelta)
                 || (that.mousePos.y > that.height/2 + that.paneWidth/2 + that.paneDelta) || (that.mousePos.x > that.width/2 + that.paneWidth/2 + that.paneDelta) ) ) ){
                 that.rayVector.set((that.mousePos.x/window.innerWidth) * 2 - 1, -(that.mousePos.y/window.innerHeight) * 2 + 1, 0.5).unproject(that.camera);
